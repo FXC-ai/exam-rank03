15ecc5e060c41aeb26d415c35ce97ed2aa4d36fd
François-Xavier <francois-xavier@MacBook-Pro-de-Francois-Xavier.local>
2023-04-09 10:45:22 +0200

    ft_printf en cours

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
 
+#define BASE_HEX "0123456789abcdef"
 
 int ft_putstr(char *str)
 {
@@ -19,20 +21,78 @@ int ft_putstr(char *str)
 
 int ft_putnbr(int nbr)
 {
-	
+	int n;
+	int count;
+	int divider;
+	int cpy_nbr;
 
+	if (nbr == -2147483648)
+	{
+		write(1,"-2147483648", 11);
+		count += 11;
+		return count;
+	}
 	if (nbr < 0)
 	{
 		write(1,"-",1);
+		count++;
 		nbr *= -1;
 	}
 
-	while (nbr > 10)
+	count = 0;
+	divider = 1;
+	cpy_nbr = nbr;
+	while (cpy_nbr > 10)
 	{
-		
-
+		cpy_nbr = cpy_nbr / 10;
+		divider = divider * 10;
 	}
 
+	while (nbr > 0)
+	{
+		n = (nbr / divider) + 48;
+		write (1,&n,1);
+		count ++;
+		nbr = nbr % divider;
+		divider /= 10;
+	}
+
+	return count;
+}
+
+int ft_putnbr_hex (int nbr, int count)
+{
+	int n = 0;
+
+	if (nbr == 0)
+	{
+		return count;
+	}
+
+
+	n = nbr % 16;
+	nbr = nbr / 16;
+	ft_putnbr_hex(nbr, count);
+	write (1,&BASE_HEX[n],1);
+	count++;
+	
+	return count;
+	/*
+	480 = 30 x 16 + 0
+	30 = 1 x 16 + 14
+	14 = 0 X 16 + 14
+	*/
+
+	/*
+	26 = 16 x 1 + 10
+	1 = 16 x 0 + 1
+	
+	*/
+
+	//printf("nbr = %d\n", nbr);
+	//count++;
+
+	//return count;
 
 }
 
@@ -79,19 +139,23 @@ int ft_printf(const char *str, ...)
 	}
 	va_end(ap);
 	
-	//printf("premier   argument : %c\n", va_arg(ap2, int));
-	//printf("deuxieme  argument : %d\n", va_arg(ap2, int));
-	//printf("troisieme argument : %s\n", va_arg(ap2, char *));
-	
 	va_end(ap2);
 	return size;
 }
 
 int main (void)
 {
-    ft_printf("Voici les tests : %c %d %s\n", 'c', 42, "hello");
+    //ft_printf("Voici les tests : %c %d %s\n", 'c', 42, "hello");
 
-	ft_putstr("Bonjour voici le test\n");
+	//ft_putstr("Bonjour voici le test\n");
+
+	int test = 2147483646;
+
+	printf("%d : %x\n", test, test);
+	
+	ft_putnbr_hex(test, 0);
+	printf("\n");
+
 
     return 0;
 }
